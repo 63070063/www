83b09ab38e3237a55318df19bbee3a0f1d925537
63070063 <73682909+63070063@users.noreply.github.com>
2020-10-30 12:45:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <h1>แนะนำตัว</h1>
         <p>ชื่อ นายตะวันไท สัจจศรี</p>
         <p>ชื่อเล่น ไม้</p>
-        <img src="kda1.jpg">
-        <img src="kda2.jpg">
-        <img src="kda3.jpg">
+        echo <img src="kda1.jpg">
+        echo <img src="kda2.jpg">
+        echo <img src="kda3.jpg">
         <br>
         <a href="https://www.facebook.com/th.lol/">This is a link</a>
         
